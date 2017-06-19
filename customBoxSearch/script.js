8642d40920abe22de76e93ceb11e0cbf39b57d73
Extension <extension@Extensions-iMac-3.local>
2017-06-19 16:25:05 -0600

    makes nav mobile and fixes search by preventing default

--- customBoxSearch/script.js
+++ customBoxSearch/script.js
@@ -30,73 +30,79 @@ $(document).ready(function(){
   });
 
 
-  // Makes department nav bar stick to top of screen when scrolling.
-  var lastScroll = 0;
 
-  // >172, >221, 0, -50
-
-
-  $(document).scroll(function(){
-    var deptNav = $('.department-nav');
-    var scrollTop = $(document).scrollTop();
-    var deptTop = deptNav.position().top;
-    if(!isShort()){
-      if(scrollTop>172){
-        if(deptNav.css('position')!=='fixed'){
-          deptNav.css({'position': 'fixed', 'top':'0'});
-        }
-      }
-      else {
-        if(deptNav.css('position')!=='static'){
-          deptNav.css({'position': 'static'});
-        }
-      }
-    } else {
-      if(scrollTop>270){
-        if(deptNav.css('position')!=='fixed'){
-          deptNav.css({'position': 'fixed', 'top':'-50px'});
-        }
-        if(scrollTop > lastScroll && deptTop == 0){ // scrolling down and bar is out
-          deptNav.animate({top:'-50px'}, 300);
-        } else if(scrollTop < lastScroll && deptTop == -50){ // scrolling up and bar is hidden
-          deptNav.animate({top:'0'}, 300);
-        }
-      }
-      else if(scrollTop<172){
-        if(deptNav.css('position')!=='static'){
-          deptNav.css({'position': 'static'});
-        }
+  if($('.department-nav').length){
+    // Makes department nav bar stick to top of screen when scrolling.
+    var lastScroll = 0;
+    var initDeptTop = $('.department-nav').position().top;
+    var initDeptBottom = $('.department-nav').position().top + $('.department-nav').outerHeight();
+    var initDeptHeight = $('.department-nav').outerHeight();
+    function recalcNavbarPos(){
+      if($('.department-nav').css('position')!=='fixed'){
+        initDeptTop = $('.department-nav').position().top;
+        initDeptBottom = $('.department-nav').position().top + $('.department-nav').outerHeight();
+        initDeptHeight = $('.department-nav').outerHeight();
+        console.log('top: '+ initDeptTop + ' bottom: '+ initDeptBottom);
       }
     }
-    lastScroll = scrollTop;
-  });
-  // else{
-  //   $(document).scroll(function(){
-  //     var deptNav = $('.department-nav');
-  //     if($(document).scrollTop()>221){
-  //       if(deptNav.css('position')!=='fixed'){
-  //         deptNav.css({'position': 'fixed', 'top':'-50px'});
-  //       }
-  //       if($(document).scrollTop()>lastScroll){
-  //         deptNav.
-  //       }
-  //       lastScroll = $(document).scrollTop();
-  //     }
-  //     else if($(document).scrollTop()<221){
-  //       if(deptNav.css('position')!=='static'){
-  //         deptNav.css({'position': 'static'});
-  //       }
-  //     }
-  //   });
-  // }
 
+    $('.navbar-collapse').on('shown.bs.collapse', function() {
+      recalcNavbarPos();
+    });
+    $('.navbar-collapse').on('hidden.bs.collapse', function(){
+      recalcNavbarPos();
+    });
+    $('.dropdown').on('shown.bs.dropdown',function(){
+      recalcNavbarPos();
+    });
+    $('.dropdown').on('hidden.bs.dropdown',function(){
+      recalcNavbarPos();
+    });
 
+    $(document).scroll(function(){
+      var deptNav = $('.department-nav');
+      var scrollTop = $(document).scrollTop();
+      var deptTop = deptNav.position().top;
+      if(!isShort()){
+        if(scrollTop>initDeptTop){
+          if(deptNav.css('position')!=='fixed'){
+            deptNav.css({'position': 'fixed', 'top':'0'});
+          }
+        }
+        else {
+          if(deptNav.css('position')!=='static'){
+            deptNav.css({'position': 'static'});
+          }
+        }
+      } else {
+        if(scrollTop>initDeptBottom){
+          if(deptNav.css('position')!=='fixed'){
+            deptNav.css({'position': 'fixed', 'top':'-'+initDeptHeight+'px'});
+          }
+          if(scrollTop > lastScroll && deptTop == 0){ // scrolling down and bar is out
+            $('.department-nav .collapse').collapse('hide');
+            deptNav.animate({top:'-'+initDeptHeight+'px'}, 300);
+          } else if(scrollTop < lastScroll && deptTop == -initDeptHeight){ // scrolling up and bar is hidden
+            deptNav.animate({top:'0'}, 300);
+          }
+        }
+        else if(scrollTop<initDeptTop){
+          if(deptNav.css('position')!=='static'){
+            deptNav.css({'position': 'static'});
+          }
+        }
+      }
+      lastScroll = scrollTop;
+    });
+  }
 
-  $('#search-btn').click(function(){
+  // search functionality
+  $('#search-btn').click(function(e){
+    e.preventDefault();
     var searchString = $('#search-input').val();
     searchString = encodeURIComponent(searchString);
     searchString = searchString.replace(/%20/g, "+");
-    console.log(searchString);
+    console.log('results.html?q='+searchString);
     window.location.href = 'results.html?q='+searchString;
   });
 });
