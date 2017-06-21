0bb9ab56f40ba9dbc0daeab71be28b6f22ec66df
Extension <extension@Extensions-iMac-3.local>
2017-06-21 08:47:10 -0600

    popup search w/ invisible gcse box

--- customBoxSearch/script.js
+++ customBoxSearch/script.js
@@ -63,68 +63,63 @@ $(document).ready(function(){
       var deptNav = $('.department-nav');
       var scrollTop = $(document).scrollTop();
       var deptTop = deptNav.position().top;
-      if(!isShort()){
+      if(window.innerHeight>660){
         if(scrollTop>initDeptTop){
           if(deptNav.css('position')!=='fixed'){
             deptNav.css({'position': 'fixed', 'top':'0'});
           }
         }
         else {
-          if(deptNav.css('position')!=='static'){
-            deptNav.css({'position': 'static'});
+          if(deptNav.css('position')!=='relative'){
+            deptNav.css({'position': 'relative'});
           }
         }
       } else {
-        if(scrollTop>initDeptBottom){
-          if(deptNav.css('position')!=='fixed'){
-            deptNav.css({'position': 'fixed', 'top':'-'+initDeptHeight+'px'});
-          }
-          if(scrollTop > lastScroll && deptTop == 0){ // scrolling down and bar is out
-            $('.department-nav .collapse').collapse('hide');
-            deptNav.animate({top:'-'+initDeptHeight+'px'}, 300);
-          } else if(scrollTop < lastScroll && deptTop == -initDeptHeight){ // scrolling up and bar is hidden
-            deptNav.animate({top:'0'}, 300);
-          }
+        if(deptNav.css('position')!=='relative'){
+          deptNav.css({'position':'relative'});
         }
-        else if(scrollTop<initDeptTop){
-          if(deptNav.css('position')!=='static'){
-            deptNav.css({'position': 'static'});
-          }
-        }
+        // if(scrollTop>initDeptBottom){
+        //   if(deptNav.css('position')!=='fixed'){
+        //     deptNav.css({'position': 'fixed', 'top':'-'+initDeptHeight+'px'});
+        //   }
+        //   if(scrollTop > lastScroll && deptTop == 0){ // scrolling down and bar is out
+        //     $('.department-nav .collapse').collapse('hide');
+        //     deptNav.animate({top:'-'+initDeptHeight+'px'}, 300);
+        //   } else if(scrollTop < lastScroll && deptTop == -initDeptHeight){ // scrolling up and bar is hidden
+        //     deptNav.animate({top:'0'}, 300);
+        //   }
+        // }
+        // else if(scrollTop<initDeptTop){
+        //   if(deptNav.css('position')!=='relative'){
+        //     deptNav.css({'position': 'relative'});
+        //   }
+        // }
       }
       lastScroll = scrollTop;
     });
   }
 
   // search functionality
-  $('#search-btn').click(function(e){
-    e.preventDefault();
-    var searchString = $('#search-input').val();
-    searchString = encodeURIComponent(searchString);
-    searchString = searchString.replace(/%20/g, "+");
-    console.log('results.html?q='+searchString);
-    window.location.href = 'results.html?q='+searchString;
+
+  // $('#search-btn').click(function(e){
+  //   e.preventDefault();
+  //   var searchString = $('#search-input').val();
+  //   searchString = encodeURIComponent(searchString);
+  //   searchString = searchString.replace(/%20/g, "+");
+  //   console.log('results.html?q='+searchString);
+  //   window.location.href = '?q='+searchString;
+  // });
+
+  // When #search-form is submitted, value of the input is copied into the invisible gcse searchbox and
+  // the click event on the gsc search button is triggered
+  $('#search-btn').click(function(){
+    $('#gsc-i-id1').val($('#search-input').val());
+    $('.gsc-search-button').trigger('click');
   });
 });
 
-function HTMLEncode(str){
-  var i = str.length,
-      aRet = [];
-
-  while (i--) {
-    var iC = str[i].charCodeAt();
-    if (iC < 65 || iC > 127 || (iC>90 && iC<97)) {
-      aRet[i] = '&#'+iC+';';
-    } else {
-      aRet[i] = str[i];
-    }
-   }
-  return aRet.join('');
-}
-
-
   (function() {
-    var cx = '002621195891316906187:mmhftt9hrp0';
+    var cx = '008637555176047886659:dyr814d8vq0';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
