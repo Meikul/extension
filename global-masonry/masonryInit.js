417579e4b98338cbc88c4c6112ea6894f5105024
Michael Harris <Meikul@users.noreply.github.com>
2017-09-25 16:54:29 -0600

    starting on msn-fill

--- global-masonry/masonryInit.js
+++ global-masonry/masonryInit.js
@@ -1,7 +1,34 @@
 $(document).ready(function(){
   $('.msn-grid').masonry({itemSelector: '.msn-item', columnWidth: '.msn-item'});
 
+  if($('.msn-item .msn-fill').length()){
+      $(window).resize(msnCheck);
+  }
+
+  $('.msn-item .msn-fill').each(function(){
+
+  });
+
   $('.item').each(function(){
     $(this).height(Math.floor(Math.random()*200 + 150));
-  })
+  });
 });
+
+function msnCheck() {
+  $('.msn-grid').each(function(){
+    const $grid = $(this);
+    const gridWidth = $grid.innerWidth();
+    const itemMinWidth = $('.msn-item .msn-fill').css('min-width');
+    var cols = Math.floor(gridWidth/itemMinWidth);
+    if (!msnCheck.lastCols) msnCheck.lastCols = cols;
+    if(msnCheck.lastCols != cols) msnFill($grid, cols);
+    msnCheck.lastCols = cols;
+  });
+}
+
+function msnFill($grid, cols){
+  const itemWidth = (100/cols)+'%';
+  $grid.find('.msn-item .msn-fill').each(function(){
+    $(this).css({width: itemWidth});
+  });
+}
