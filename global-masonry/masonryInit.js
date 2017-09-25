61cd9912a16af9017ca6212353afb3deabdab3fd
Michael Harris <Meikul@users.noreply.github.com>
2017-09-25 15:35:26 -0600

    playing with grid-sizer

--- global-masonry/masonryInit.js
+++ global-masonry/masonryInit.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
   $('.msn-grid').masonry({itemSelector: '.msn-item', columnWidth: '.msn-item'});
+
+  $('.item').each(function(){
+    $(this).height(Math.floor(Math.random()*200 + 150));
+  })
 });
