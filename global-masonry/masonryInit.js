8ff85a1388f41cb5e529070e25f744e4345ffa9b
Meikul <meikul.harris@gmail.com>
2017-09-27 11:58:36 -0600

    first stable version, probably doesn't work with multiple grids

--- global-masonry/masonryInit.js
+++ global-masonry/masonryInit.js
@@ -1,34 +1,43 @@
 $(document).ready(function(){
   $('.msn-grid').masonry({itemSelector: '.msn-item', columnWidth: '.msn-item'});
 
-  if($('.msn-item .msn-fill').length()){
-      $(window).resize(msnCheck);
+  console.log($('.msn-item.msn-fill').length);
+  if($('.msn-item.msn-fill').length){
+    $(window).resize(msnCheck);
+    msnCheck();
   }
 
-  $('.msn-item .msn-fill').each(function(){
-
-  });
-
+  // REMOVE
   $('.item').each(function(){
     $(this).height(Math.floor(Math.random()*200 + 150));
-  });
+  }).parent('.msn-grid').masonry();
+
 });
+
+// Make array in msnCheck that holds thresholds then use incrementor in .each()
 
 function msnCheck() {
   $('.msn-grid').each(function(){
     const $grid = $(this);
     const gridWidth = $grid.innerWidth();
-    const itemMinWidth = $('.msn-item .msn-fill').css('min-width');
-    var cols = Math.floor(gridWidth/itemMinWidth);
-    if (!msnCheck.lastCols) msnCheck.lastCols = cols;
+    const $item = $('.msn-item.msn-fill')
+    const itemMinWidth = parseFloat($item.css('min-width'), 10);
+    const itemMargin = $item.outerWidth(true) - $item.innerWidth();
+    var cols = Math.floor(gridWidth/(itemMinWidth+itemMargin));
     if(msnCheck.lastCols != cols) msnFill($grid, cols);
     msnCheck.lastCols = cols;
   });
 }
 
 function msnFill($grid, cols){
-  const itemWidth = (100/cols)+'%';
-  $grid.find('.msn-item .msn-fill').each(function(){
-    $(this).css({width: itemWidth});
+  var itemWidth;
+  $grid.find('.msn-item.msn-fill').each(function(){
+    var $item = $(this)
+    var margin = $item.outerWidth(true) - $item.innerWidth();
+    if (margin) itemWidth = 'calc('+(100/cols)+'% - '+margin+'px)';
+    else itemWidth = (100/cols)+'%';
+    $item.css({width: itemWidth});
   });
+  // $grid.find('.grid-sizer').css({width: 100/cols+'%'});
+  $grid.masonry();
 }
