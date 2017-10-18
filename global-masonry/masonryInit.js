c6c38c05a667328bc41ae9bab6ad0bec8c8dd4f7
Meikul <meikul.harris@gmail.com>
2017-10-17 18:01:45 -0600

    stable with multiple grids and moves msn-fill from item to grid

--- global-masonry/masonryInit.js
+++ global-masonry/masonryInit.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
   $('.msn-grid').masonry({itemSelector: '.msn-item', columnWidth: '.msn-item'});
 
-  console.log($('.msn-item.msn-fill').length);
-  if($('.msn-item.msn-fill').length){
+  msnCheck.lastCols=[];
+  console.log($('.msn-grid.msn-fill .msn-item').length);
+  if($('.msn-grid.msn-fill .msn-item').length){
     $(window).resize(msnCheck);
     msnCheck();
   }
+
 
   // REMOVE
   $('.item').each(function(){
@@ -17,27 +19,27 @@ $(document).ready(function(){
 // Make array in msnCheck that holds thresholds then use incrementor in .each()
 
 function msnCheck() {
-  $('.msn-grid').each(function(){
+  $('.msn-grid.msn-fill').each(function(i){
     const $grid = $(this);
     const gridWidth = $grid.innerWidth();
-    const $item = $('.msn-item.msn-fill')
+    const $item = $grid.find('.msn-item');
     const itemMinWidth = parseFloat($item.css('min-width'), 10);
     const itemMargin = $item.outerWidth(true) - $item.innerWidth();
     var cols = Math.floor(gridWidth/(itemMinWidth+itemMargin));
-    if(msnCheck.lastCols != cols) msnFill($grid, cols);
-    msnCheck.lastCols = cols;
+    if(msnCheck.lastCols[i] != cols) msnFill($grid, cols);
+    msnCheck.lastCols[i] = cols;
   });
 }
 
 function msnFill($grid, cols){
   var itemWidth;
-  $grid.find('.msn-item.msn-fill').each(function(){
+  $grid.find('.msn-item').each(function(){
     var $item = $(this)
     var margin = $item.outerWidth(true) - $item.innerWidth();
     if (margin) itemWidth = 'calc('+(100/cols)+'% - '+margin+'px)';
     else itemWidth = (100/cols)+'%';
     $item.css({width: itemWidth});
   });
-  // $grid.find('.grid-sizer').css({width: 100/cols+'%'});
+  // $grid.find('.msn-grid-sizer').css({width: 100/cols+'%'});
   $grid.masonry();
 }
