2774e5b73cf95e3fb669ffb6fb16741bdbda70db
Michael Harris <michael@Michaels-MacBook-Pro.local>
2018-06-22 13:38:22 -0600

    drip integrated with ycb

---
+++ ycb/drip-header.js
@@ -0,0 +1,11 @@
+var _dcq = _dcq || [];
+var _dcs = _dcs || {};
+_dcs.account = '6932252';
+(function () {
+  var dc = document.createElement('script');
+  dc.type = 'text/javascript';
+  dc.async = true;
+  dc.src = '//tag.getdrip.com/6932252.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(dc, s);
+})();
